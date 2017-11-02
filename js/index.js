a0e9092c78dad8b94382a500b2a85322ab88500d
Your Name <1142768306@qq.com>
2017-11-02 10:09:14 +0800

    add login page

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// $(function(){
+// 	$('a').click(function(){
+
+//     //getAjax("get","www.abc.com",{name:"wfb",age:20});
+		
+	
+// 	})
+// })
+
+
+
+// function getAjax(newType,newUrl,newData){
+// 	$.ajax({
+//            type: newType,
+//            url: newUrl,
+//            dataType: 'json',
+//            data: newData,
+// 	        success: function (result) {
+//                if (result.status == 0) {
+                   
+//                    window.location.href='four_modules.html';
+
+//                }
+//                else {
+//                    console.log('登录失败');
+//                }
+//            },
+//            err: function () {
+//                console.log('登录失败');
+//            }
+//        })
+// }
+		
+		
